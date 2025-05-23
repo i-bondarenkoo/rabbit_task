7be57440b804295f58e6ca0915bcf0f696aeddba
Igor Bondarenko <bondarenko200021@mail.ru>
2025-05-23 11:28:09 +0700

    upd dec

--- decorators.py
+++ decorators.py
@@ -1,75 +1,72 @@
 import time
-from unittest import result
 
 
-# list_words = ["banana", "pineapple", "12", "jin", "cucumber"]
+list_words = ["banana", "pineapple", "12", "jin", "cucumber"]
 
 
-# def log_call(verbose):
-#     def get_name(func):
-#         def wrapper(*args, **kwargs):
-#             if verbose:
-#                 print(
-#                     f"Вызов функции: {func.__name__} c аргументами {','.join(map(str, args))}"
-#                 )
+def log_call(verbose):
+    def get_name(func):
+        def wrapper(*args, **kwargs):
+            if verbose:
+                print(
+                    f"Вызов функции: {func.__name__} c аргументами {','.join(map(str, args))}"
+                )
 
-#             return func(*args, **kwargs)
+            return func(*args, **kwargs)
 
-#         return wrapper
+        return wrapper
 
-#     return get_name
-
-
-# @log_call(verbose=True)
-# def filter_and_uppercase_words(list_words: list):
-#     return [item.upper() for item in list_words if len(item) > 3]
+    return get_name
 
 
-# def get_time(func):
-#     def wrapper(*args, **kwargs):
-#         start = time.time()
-#         result = func(*args, **kwargs)
-#         time.sleep(2)
-#         stop = time.time() - start
-#         return f"Время выполнения функции составило: {stop}, результат: {result}"
-
-#     return wrapper
+@log_call(verbose=True)
+def filter_and_uppercase_words(list_words: list):
+    return [item.upper() for item in list_words if len(item) > 3]
 
 
-# @get_time
-# def pow_digit(list_digit):
-#     return [x**2 for x in list_digit]
+def get_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        time.sleep(2)
+        stop = time.time() - start
+        return f"Время выполнения функции составило: {stop}, результат: {result}"
+
+    return wrapper
 
 
-# def file_name(name_f):
-#     def real_decorator(func):
-#         def wrapper(*args, **kwargs):
-#             if isinstance(name_f, str):
-#                 filename = name_f if name_f.endswith(".txt") else name_f + ".txt"
-#                 with open(file=filename, mode="w", encoding="utf-8") as file:
-#                     result = f"Название вызванной функции: {func.__name__}, время вызова функции: {time.time()}"
-#                     file.write(result)
-#             return func(*args, **kwargs)
-
-#         return wrapper
-
-#     return real_decorator
+@get_time
+def pow_digit(list_digit):
+    return [x**2 for x in list_digit]
 
 
-# @file_name(name_f="report")
-# def get_len_string(list_string):
-#     return [len(elem) for elem in list_string]
+def file_name(name_f):
+    def real_decorator(func):
+        def wrapper(*args, **kwargs):
+            if isinstance(name_f, str):
+                filename = name_f if name_f.endswith(".txt") else name_f + ".txt"
+                with open(file=filename, mode="w", encoding="utf-8") as file:
+                    result = f"Название вызванной функции: {func.__name__}, время вызова функции: {time.time()}"
+                    file.write(result)
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return real_decorator
 
 
-# def make_multiplier(n):
-#     n += 1
+@file_name(name_f="report")
+def get_len_string(list_string):
+    return [len(elem) for elem in list_string]
 
-#     def inner(x):
-#         # nonlocal n
-#         result = x * n
-#         return result
 
-#     return inner
+def make_multiplier(n):
+
+    def inner(x):
+        result = x * n
+        return result
+
+    return inner
 
 
 def call_counter(func):
@@ -89,17 +86,17 @@ def greet(name):
 
 
 if __name__ == "__main__":
-    # print(pow_digit([2, 7, -4, 2, 0, 5]))
-    # result = get_time(pow_digit)
-    # print(result([2, 7, -4, 2, 0, 5]))
-    # print(get_len_string(["a", "sasad", "sadasdasdads"]))
-    # test_decorator = file_name("report")(get_len_string)
-    # print(test_decorator(["a", "sasad", "sadasdasdads"]))
-    # test = make_multiplier(3)
-    # print(test(5))
-    # print(test(7))
-    # print(greet("Jopa"))
-    # print(greet("123"))
+    print(pow_digit([2, 7, -4, 2, 0, 5]))
+    result = get_time(pow_digit)
+    print(result([2, 7, -4, 2, 0, 5]))
+    print(get_len_string(["a", "sasad", "sadasdasdads"]))
+    test_decorator = file_name("report")(get_len_string)
+    print(test_decorator(["a", "sasad", "sadasdasdads"]))
+    test = make_multiplier(3)
+    print(test(5))
+    print(test(7))
+    print(greet("Jopa"))
+    print(greet("123"))
     test_func = call_counter(greet)
     print(test_func("stepa"))
     print(test_func("grisha"))
